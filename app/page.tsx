45f9086ce9463a7a54c4c73db60e1a6141df68de
enhhtogtoh <enhhtogtoh@gmail.com>
2026-05-27 11:43:15 +0800

    broken test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+// export default function Home() {
+//   return (
+//     <main className="min-h-screen flex flex-col items-center justify-center">
+//       <h1 className="text-5xl font-bold">CI/CD test</h1>
+
+//       <p className="mt-4 text-gray-600">GitHub Actions + Vercel ajillaj bn</p>
+
+//       <button className="mt-6 px-6 py-3 rounded-lg bg-black text-white">
+//         Deploy Test
+//       </button>
+//     </main>
+//   );
+// }
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold">CI/CD test</h1>
-
-      <p className="mt-4 text-gray-600">GitHub Actions + Vercel ajillaj bn</p>
-
-      <button className="mt-6 px-6 py-3 rounded-lg bg-black text-white">
-        Deploy Test
-      </button>
+    <main>
+      <h1>{undefinedVariable}</h1>
     </main>
   );
 }
