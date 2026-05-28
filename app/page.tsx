c85acb9f2c5d4f7ca34ce145845573f3c5f36d1c
enhhtogtoh <enhhtogtoh@gmail.com>
2026-05-28 09:45:17 +0800

    final demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,41 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">CI/CD test</h1>
+
+      <p className="mt-4 text-gray-600">GitHub Actions + Vercel ajillaj bn</p>
+
+      <button className="mt-6 px-6 py-3 rounded-lg bg-black text-white">
+        Deploy Test
+      </button>
+    </main>
+  );
+}
+
 // export default function Home() {
+//   const user: any = null;
+
 //   return (
-//     <main className="min-h-screen flex flex-col items-center justify-center">
-//       <h1 className="text-5xl font-bold">CI/CD test</h1>
+//     <main className="min-h-screen flex items-center justify-center">
+//       <div>
+//         <h1> Bug test</h1>
 
-//       <p className="mt-4 text-gray-600">GitHub Actions + Vercel ajillaj bn</p>
-
-//       <button className="mt-6 px-6 py-3 rounded-lg bg-black text-white">
-//         Deploy Test
-//       </button>
+//         <p>{user.name}</p>
+//       </div>
 //     </main>
 //   );
 // }
 
-export default function Home() {
-  const user = { name: "Tok" };
+// export default function Home() {
+//   const user= { name: "Tok" };
 
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div>
-        <h1> Fixed</h1>
+//   return (
+//     <main className="min-h-screen flex items-center justify-center">
+//       <div>
+//         <h1> Fixed test</h1>
 
-        <p>{user.name}</p>
-      </div>
-    </main>
-  );
-}
+//         <p>{user.name}</p>
+//       </div>
+//     </main>
+//   );
+// }
